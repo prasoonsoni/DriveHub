62c96530c4ac54a8d716a229073d63e5cde62f4d
Prasoon Soni <prasoonsoni03@gmail.com>
2023-02-13 17:55:03 +0530

    Added Firebase Config

---
+++ client/src/config/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+import { getStorage } from "firebase/storage"
+const { REACT_APP_APIKEY, REACT_APP_AUTHDOMAIN, REACT_APP_PROJECTID, REACT_APP_STORAGEBUCKET, REACT_APP_MESSAGINGSENDERID, REACT_APP_APPID, REACT_APP_MEASUREMENTID } = process.env
+
+const firebaseConfig = {
+    apiKey: REACT_APP_APIKEY,
+    authDomain: REACT_APP_AUTHDOMAIN,
+    projectId: REACT_APP_PROJECTID,
+    storageBucket: REACT_APP_STORAGEBUCKET,
+    messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+    appId: REACT_APP_APPID
+}
+
+const app = initializeApp(firebaseConfig)
+const storage = getStorage(app)
+
+export { app, storage }
